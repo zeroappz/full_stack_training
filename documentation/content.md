f7106e638e9d207609ff9a69728207b7dddcc774
AlgoxFusion <72295783+zeroappz@users.noreply.github.com>
2024-03-18 16:50:14 +0530

    [Feat]: Signin & signup components

---
+++ documentation/content.md
@@ -0,0 +1,22 @@
+# Regular Expression
+Characters:
+1. numbers as "123" ==> "123" ==> "[0-9]{3}" as "\d{3}" 
+2. "406004" ==> [0-9]{6}
+3. Alphabets (LC) ==> [a-z]{6, 12}
+4. Alphabets (UC) ==> [A-Z]{3, }
+5. Password Combination ==> [a-zA-Z0-9!@#$%&]{8, 16}
+6. Mobile Number ==> "9047048344" ==> "[6789]{1}[0-9]{9}" ==> "[6789][0-9]{9}" ==> "[6|7|8|9][0-9]{9}"
+7. IP Address ==> "89.0.142.86" ==> "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+
+Meta-Characters:
+1. "." ==> "." ==> "."; eg "aaa" ==> "..." (any element can be present)
+2. "+" ==> "+" ==> "+"; eg "aaa" ==> "a+" (at least one element can be present)
+3. "*" ==> "*" ==> "*"; eg "aaa" ==> "a*" (Zero or more element can be present)
+4. \d ==> "[0-9]" ==> "[0-9]"; eg "123" ==> "\d{3}" 
+5. \w ==> "[a-zA-Z0-9]" ==> "[a-zA-Z0-9]"; eg "abc" ==> "\w{3}"
+
+Quantity:
+1. min & max ==> {6, 12}
+2. min is 1 and max can be of anything ==> {1, } - minimum 1 is mandatory
+3. min is 0 and max can be of anything ==> {0, } - Optional
+Grouping
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { NgIf } from '@angular/common';
+import { NgFor, NgForOf, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -8,12 +8,19 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [
     ReactiveFormsModule,
-    NgIf
+    NgIf,
+    NgFor,
+    NgForOf
   ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+
+  // country List
+  countries = ['India', 'USA', 'UK', 'Australia', 'Canada', 'Japan', 'China'];
+  // Gender
+  genders = ['Male', 'Female', 'Others'];
 
   constructor(private router: Router) { }
 
@@ -24,14 +31,18 @@ export class SignupComponent {
 
   signUpForm = new FormGroup({
     firstName: new FormControl(
-      'Praveen', Validators.required
+      'Praveen'
     ),
     lastName: new FormControl('Kumar', Validators.required),
     age: new FormControl(Validators.required),
     email: new FormControl('test@gmail.com', Validators.required),
-    password: new FormControl(),
-    confirmPassword: new FormControl()
+    password: new FormControl(Validators.pattern('[a-zA-Z0-9]{6, 12}'),),
+    confirmPassword: new FormControl(Validators.pattern('[a-zA-Z0-9]{6, 12}')),
+    gender: new FormControl('Male', Validators.required),
+    country: new FormControl('India', Validators.required),
+    address: new FormControl('XYZ, Pune', Validators.required)
   });
+
 
   signInNav() {
     // window.location.href = 'signup'; // using JS window.location
@@ -39,6 +50,12 @@ export class SignupComponent {
   }
 
   onSubmit() {
+    // check the validation
+    if (this.signUpForm.invalid) {
+      console.log('Invalid Form');
+      console.log(this.signUpForm.value);
+      return;
+    }
     console.log(this.signUpForm.value);
     // confirm password
     if (this.signUpForm.value.password === this.signUpForm.value.confirmPassword) {
